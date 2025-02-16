2a41b5879edd630e2fdca552a4d13cfcb23b0123
Santhosh Raj V <140722890+Santhoshraj2730@users.noreply.github.com>
2025-02-16 17:29:18 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
-import os 
-from app import app
-if __name__ == "__main__":
-    port=int(os.environ.get("PORT",4000))
-    app.run(host="0.0.0.0",port=port)
+import os  
+from app import app  
+
+if __name__ == "__main__":  
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 locally  
+    app.run(host="0.0.0.0", port=port, debug=True)  
